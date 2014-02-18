b9d53fdd4ed24b3facc0749e5167a6c7eb56ace2
yong <448709056@qq.com>
2014-02-18 16:14:34 +0800

    test

---
+++ gengzongzun/src/src/java/com/pingan/parking/ParkingBoy.java
@@ -0,0 +1,11 @@
+package src.java.com.pingan.parking;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zz
+ * Date: 14-2-18
+ * Time: 下午3:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParkingBoy {
+}
---
+++ gengzongzun/src/src/test/com/pingan/parking/ParkingBoyTest.java
@@ -0,0 +1,11 @@
+package src.test.com.pingan.parking;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zz
+ * Date: 14-2-18
+ * Time: 下午3:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParkingBoyTest {
+}
