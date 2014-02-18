825c1e6e5c69d610a2f07f39b5ce90ce63f94cf1
yong <448709056@qq.com>
2014-02-18 14:25:32 +0800

    test

---
+++ gengzongzun/src/java/com/pingan/parking/parking/Parking.java
@@ -0,0 +1,32 @@
+package com.pingan.parking.parking;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parking {
+    private int capacity;
+    private Map<String, Car> carMap;
+    public Parking(int capacity){
+        if(capacity <=0){
+            throw  new RuntimeException("capacity must > 0!");
+        }
+        carMap = new HashMap<String, Car>();
+        this.capacity = capacity;
+    }
+    public boolean parkCar(Car car){
+        if(!isFull()){
+            carMap.put(car.getCarNumber(), car);
+            return true;
+        }
+        return false;
+    }
+    private boolean isFull(){
+        return carMap.size() >= capacity;
+    }
+    public boolean takeCar(String carNumber){
+        if(carMap.get(carNumber)!=null){
+            carMap.remove(carNumber);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ gengzongzun/src/java/com/pingan/parking/parking/parking/parking/Car.java
@@ -0,0 +1,11 @@
+package com.pingan.parking.parking.parking.parking;
+
+public class Car {
+    private String carNumber;
+    public Car(String carNumber) {
+        this.carNumber = carNumber;
+    }
+    public String getCarNumber() {
+        return carNumber;
+    }
+}
