9a6bdf586d1abc9b93ac369f35ed7411da709113
yong <448709056@qq.com>
2014-02-18 15:03:23 +0800

    test

---
+++ gengzongzun/src/src/java/com/pingan/parking/Car.java
@@ -0,0 +1,11 @@
+package src.java.com.pingan.parking;
+
+public class Car {
+    private String carNumber;
+    public Car(String carNumber) {
+        this.carNumber = carNumber;
+    }
+    public String getCarNumber() {
+        return carNumber;
+    }
+}
